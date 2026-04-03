02e24f24850f845b8ce72cf42a8a00c6aca9a577
esdeatn <orarosfa@gmail.com>
2026-04-03 10:29:50 +0500

    fix: use DATABASE_URL for production

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "music_app",
-  password: "1234",
-  port: 5432,
-});
+export const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: process.env.DATABASE_URL.includes("railway.internal")
+          ? false
+          : { rejectUnauthorized: false },
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "music_app",
+        password: "1234",
+        port: 5432,
+      }
+);
